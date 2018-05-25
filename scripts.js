6f70d779f7df1926a2d722c54c69a9725997b5ee
Sandeep Sihari <kumarsandeep91@gmail.com>
2018-05-25 20:31:49 +0530

    JS object demo

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// TODO: create a object of your choice with required properties and methods to describe yourself like name, age, address, etc
+// Take hint from below syntax.
+/*
+var myObject = {
+    // define neccesary properties and methods 
+}
+*/
+
+function introduceMe() {
+    // Take hint from below syntax and use the same to introduce yourself on the webpage.
+    // document.body.innerHTML += '<p>Sandeep Kumar describes himself: ' + myObject.describeYourself(); + '</p>';  
+    // document.body.innerHTML += '<p>Dark Vador says: ' + myObject.talk(); + '</p>';
+}
